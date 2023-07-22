546540575715bf60129148cb398f56f1e5df0877
programming hero <naofelbadsha@gmail.com>
2023-07-22 10:22:59 +0100

    added banner

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
-      extend: {},
+      extend: {
+        color:{
+          clifford: '#da373d',
+          'bike-primary': '#e76f51',
+          'bike-primary-bg': 'rgba(231, 111, 81, 0.10)',
+        }
+      },
     },
     plugins: [],
   }
